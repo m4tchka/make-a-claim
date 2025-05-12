19621261d8c98abf6c77fb9c52e49b254d946dd5
m4tchka <91279022+m4tchka@users.noreply.github.com>
2025-05-12 17:21:02 +0100

    add working auth to landing page

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,19 +1,92 @@
 import "./LandingPage.css";
-import { auth } from "../../firebase.js";
+import { auth } from "../../Firebase.js";
 import { useState } from "react";
 import { Link } from "react-router";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
 export default function LandingPage() {
   const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser ? true : false);
+  const [emailInput, setEmailInput] = useState("");
+  const [passwordInput, setPasswordInput] = useState("");
+
+  async function signIn() {
+    await signInWithEmailAndPassword(auth, emailInput, passwordInput)
+      .then((userCredential) => {
+        // Signed in successfully
+        const user = userCredential.user;
+        console.log("Signed in as:", user.email);
+      })
+      .then(() => {
+        setIsLoggedIn(true);
+      })
+      .catch((error) => {
+        console.error("Sign in error:", error.message);
+      });
+  }
+  async function signUp() {
+    await createUserWithEmailAndPassword(auth, emailInput, passwordInput)
+      .then((userCredential) => {
+        // Signed up successfully
+        const user = userCredential.user;
+        console.log("Signed up as:", user.email);
+      })
+      .catch((error) => {
+        console.error("Sign up error:", error.message);
+      });
+  }
+
   return (
     <>
-      <div>landingpage</div>
-      {isLoggedIn && (
-        <Link to="/dashboard">
-          <button>Dashboard</button>
-        </Link>
-      )}
-      {isLoggedIn ? <p>User: {auth.currentUser.uid}</p> : <p>No user found</p>}
+      <section>
+        <h1>Welcome</h1>
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          onChange={(e) => {
+            setEmailInput(e.target.value);
+          }}
+          value={emailInput}
+        ></input>
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          onChange={(e) => {
+            setPasswordInput(e.target.value);
+          }}
+          value={passwordInput}
+        ></input>
+        <button
+          onClick={() => {
+            signIn();
+          }}
+        >
+          Sign In
+        </button>
+        <button
+          onClick={() => {
+            signUp();
+          }}
+        >
+          Sign Up
+        </button>
+
+        <div>landingpage</div>
+        {isLoggedIn && (
+          <Link to="/dashboard">
+            <button>Dashboard</button>
+          </Link>
+        )}
+        {isLoggedIn ? (
+          <p>User: {auth.currentUser.uid}</p>
+        ) : (
+          <p>No user found</p>
+        )}
+      </section>
     </>
   );
 }
